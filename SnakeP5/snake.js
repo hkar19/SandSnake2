74147f76e84e63d9d7971d801e469b17b891c0ab
hkar <hadi.kurniawan@sci.ui.ac.id>
2018-09-16 22:22:40 +0700

    i do not yet apply Snake.eat().
    i changed sketch so when the snake is out of canvas, the snake is 
    appeared on the other side of canvas.
    
    i also modified how the sketch draws in this way:
    before=
    1. snake shows
    2. snake moves
    3. food shows
    4. check whether snake eats food
    
    after:
    1. snake shows
    2. food shows
    3. check whether snake eats food
    4. ular moves
    5. if snake is offgrid, snake reappears on the other side.
    
    
    before the snake may slipped the food even though the snake clearly 
    passed the food.

--- SnakeP5/snake.js
+++ SnakeP5/snake.js
@@ -1,6 +1,11 @@
 // this snake wont move the opposite of its direction.
 // this snake will compare previous direction and proposed direction
 // if opposed, do previous direction.
+//
+// when snake eats, it will adds one tail
+// the head moves and all old tails will follow.
+// new tail will be added to the end
+
 
 function Snake(unit){
   this.size = unit;
@@ -16,6 +21,9 @@ function Snake(unit){
     fill(255);
     rect(this.xPos,this.yPos,this.size,this.size);
   }
+  //this.eat = function(){
+
+//  }
 
   this.move = function(x,y){
     if(this.prvX ==-1*x) this.xPos += this.prvX*this.speed;
@@ -23,7 +31,7 @@ function Snake(unit){
       this.xPos += x*this.speed;
       this.prvX = x;
     }
-    
+
 
     if(this.prvY == -1*y) this.yPos += this.prvY*this.speed;
     else{
